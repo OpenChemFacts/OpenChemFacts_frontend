d49e59b72e2d77dd996f8483fbdfa83540d0faad
Alban Fournier <al.fournierchavoix@gmail.com>
2025-11-24 22:08:07 +0100

    Header fixed = always displaed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { Logo } from "@/components/Logo";
 
 export const Header = () => {
   return (
-    <header className="border-b bg-card">
+    <header className="sticky top-0 z-50 border-b bg-card/95 backdrop-blur supports-[backdrop-filter]:bg-card/80">
       <div className="container mx-auto px-4 py-5">
         <div className="flex items-center justify-between">
           <Logo />
